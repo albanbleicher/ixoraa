35e91cfc5aa308a87e27db8008cdbf5eaf124ce6
Alban Bleicher <alban.b@thewalkingnerds.com>
2021-03-18 17:30:11 +0100

    edit port for heroku ??

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const app = require('express')();
 const http = require('http').Server(app);
 const Handler = require('./src/Handler').default
+const port = process.env.PORT || 3000
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
 
-http.listen(3000, () => {
+http.listen(port, () => {
   console.log('listening on *:3000');
   const Socket = new Handler(http)
   Socket.init()
