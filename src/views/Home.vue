ffabdfe5fdc26005c2d0bf0da0ac2966dd460400
Alexis Sejourné <alexis.sejourne@edu.gobelins.fr>
2021-04-20 10:23:49 +0200

    Add checks on button note and start multiple lines

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,62 +1,71 @@
 <template>
-  <div class="game">
-    <div ref="shape" class="shape"></div>
-  </div>
+  <main>
+    <h1>Computer</h1>
+    <div class="controller" v-if="!waveTime">
+      <button @click="handleMove('up')">up</button>
+      <div class="aside">
+        <button @click="handleMove('left')">left</button>
+        <button @click="handleMove('right')">right</button>
+      </div>
+      <button @click="handleMove('down')">down</button>
+    </div>
+    <div class="musicontroller">
+      <button class="musicbutton red" id="red" ref="red"></button>
+    </div>
+  </main>
 </template>
 <script>
 import io from "socket.io-client";
 import gsap from "gsap";
+
 export default {
   data() {
     return {
       io: null,
-      positions: {
-        x: 0,
-        y: 0,
-      },
+      musicPatern: null,
+      //melodyPlayed: [],
+      //musicState: "",
+      waveTime: "",
     };
   },
   created() {
+    //localStorage.debug = '*';
+    //this.io = io("http://ixoraa-api.herokuapp.com/");
     this.io = io("http://localhost:3000");
-    console.log(this.io);
   },
   mounted() {
-    this.io.on("move up", () => {
-      this.handleMove("up");
-    });
-    this.io.on("move down", () => {
-      this.handleMove("down");
-    });
-    this.io.on("move left", () => {
-      this.handleMove("left");
-    });
-    this.io.on("move right", () => {
-      this.handleMove("right");
+    this.io.on("musictime begin", (time) => {
+      this.waveTime = time;
+      this.handleMusicTimeBegin();
     });
   },
   methods: {
     handleMove(direction) {
-      const shape = this.$refs["shape"];
-      const timeline = gsap.timeline();
       switch (direction) {
         case "up":
-          this.positions.y -= 30;
-          timeline.to(shape, { y: this.positions.y });
+          this.io.emit("move up");
           break;
         case "down":
-          this.positions.y += 30;
-          timeline.to(shape, { y: this.positions.y });
+          this.io.emit("move down");
           break;
         case "left":
-          this.positions.x -= 30;
-          timeline.to(shape, { x: this.positions.x });
+          this.io.emit("move left");
           break;
         case "right":
-          this.positions.x += 30;
-          timeline.to(shape, { x: this.positions.x });
+          this.io.emit("move right");
           break;
       }
+    },
+    handleMusic(color) {
+      /*if (this.melodyPlayed.length === 5) {
+        if (
+          JSON.stringify(this.melodyPlayed) === JSON.stringify(this.waveTime)
+        )
+          this.musicState = "Correct !";
+        else this.musicState = "Oh boy, no";
+      }*/
     },
   },
 };
 </script>
+
--- src/views/MusicTime.vue
+++ src/views/MusicTime.vue
@@ -1,6 +1,26 @@
 <template>
   <div class="game">
-    <div ref="line" class="line"></div>
+    <span
+      v-for="line in lines"
+      :key="line.id"
+      class="musicbutton line"
+      id="line"
+      :ref="'line' + line.id"
+    ></span>
+    <!--<span class="musicbutton line" id="line" ref="line"></span>-->
+
+    <div>
+      <button class="note" ref="note" @click="playNote()"></button>
+    </div>
+    <div>
+      <p
+        v-for="(attempt, index) in attempts"
+        :key="index"
+        :ref="'attempt' + index"
+      >
+        {{ attempt }}
+      </p>
+    </div>
   </div>
 </template>
 <script>
@@ -14,6 +34,8 @@ export default {
         x: 0,
         y: 0,
       },
+      lines: [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }, { id: 5 }],
+      attempts: [],
       waveTime: "",
     };
   },
@@ -32,15 +54,53 @@ export default {
   },
   methods: {
     handleMove() {
-      const line = this.$refs["line"];
+      this.lines.forEach((el) => {
+      const line = this.$refs["line" + el.id];
+      //const line = this.$refs["line"];
+      console.log(this.$refs);
       const timeline = gsap.timeline();
       setTimeout(() => {
         timeline.to(line, {
           width: 1200,
           height: 1200,
-          duration: this.waveTime,
+          duration: this.waveTime * 4,
         });
-      }, this.waveTime / 8 * 1000);
+      }, (this.waveTime / el.id) * 2 * 1000);
+      });
+
+      // 8 is the good value
+    },
+    playNote() {
+      this.lines.forEach((el) => {
+        const identifier = "line" + el.id;
+        console.log(identifier);
+        const line = this.$refs[identifier];
+        const note = this.$refs["note"];
+        console.log(line);
+
+        line.offsetBottom = line.offsetTop + line.offsetHeight;
+        line.offsetRight = line.offsetLeft + line.offsetWidth;
+        note.offsetBottom = note.offsetTop + note.offsetHeight;
+        note.offsetRight = note.offsetLeft + note.offsetWidth;
+
+        //console.log('line.offsetbottom :' + line.offsetBottom, 'note.offsettop :' + note.offsetTop, 'note.offsetbottom :' + note.offsetBottom, 'line.offsetBottom < note.offsetTop :' + line.offsetBottom < note.offsetTop, 'line.offsetBottom > note.offsetBottom: ', line.offsetBottom > note.offsetBottom)
+
+        const check = !(
+          (
+            line.offsetBottom < note.offsetTop ||
+            line.offsetBottom > note.offsetBottom
+          )
+          // line.offsetTop > note.offsetBottom ||
+          // line.offsetRight < note.offsetLeft ||
+          // line.offsetLeft > note.offsetRight
+        );
+        console.log(check);
+        if (check) {
+          this.attempts.push("Correct !");
+        } else {
+          this.attempts.push("False, noob");
+        }
+      });
     },
   },
 };
