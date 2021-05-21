d4ab1bd853e8dcb4d0d6dae38f1d71bb20688a72
SEJOURNÉ Alexis <asejourne@g-a102a-15.local>
2021-05-21 11:39:48 +0200

    Try monolith color

--- src/js/World/Planet.js
+++ src/js/World/Planet.js
@@ -4,10 +4,12 @@ import {
   BoxGeometry,
   Vector3,
   MeshNormalMaterial,
+  MeshPhongMaterial,
+  MixOperation
 } from 'three'
 import Totem from './Totem'
 import Random from '../Tools/Random'
-import { MeshBasicMaterial } from 'three/build/three.module'
+import { MeshBasicMaterial, MeshPhysicalMaterial } from 'three/build/three.module'
 export default class Planet {
   constructor(params) {
     // params
@@ -29,28 +31,28 @@ export default class Planet {
   init() {
     const geometry = new BoxGeometry(1000, 1000, 0.1)
     const material = new MeshStandardMaterial({
-      color:'#9E3C74',
-      roughness:0.6
+      color: '#9E3C74',
+      roughness: 0.6
     })
 
     this.mesh = this.assets.models.ground.scene
     this.mesh.material = material
-    this.ground = this.mesh.children.find(item => item.name ==="map_rework")
+    this.ground = this.mesh.children.find(item => item.name === "map_rework")
     this.ground.material = material
-    this.mesh.position.y=-10
-    this.mesh.traverse((obj)=> {
+    this.mesh.position.y = -10
+    this.mesh.traverse((obj) => {
       obj.receiveShadow = true
       obj.castShadow = true
     })
-   this.container.add(this.mesh)
-   this.setMaterials()
+    this.container.add(this.mesh)
+    this.setMaterials()
   }
   setTotems(count) {
     for (let i = 0; i < count; i++) {
       const x = Random(-500, 500)
       const z = Random(-500, 500)
       const totem = new Totem({
-        position: new Vector3(20, 0,20 ),
+        position: new Vector3(20, 0, 20),
         time: this.time,
         sounds: this.sounds
       })
@@ -59,9 +61,12 @@ export default class Planet {
   }
   setMaterials() {
     const MONOLITHE = this.mesh.children.find(item => item.name === 'gro_monolithe')
-    const material_monolithe = new MeshStandardMaterial({
-      color:'black',
-      metalness:1
+    const material_monolithe = new MeshPhongMaterial({
+      //color: 0x5e5e5e,
+      //reflectivity: 1,
+      shininess: 100,
+      emissive: 0x87A85f,
+      roughness: 1,
     })
     MONOLITHE.material = material_monolithe
   }
