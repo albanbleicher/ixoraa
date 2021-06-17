717ecd0849501696ae303eb1662a6d15056d881a
Alban Bleicher <alban.b@thewalkingnerds.com>
2021-06-17 15:00:38 +0200

    correct index.js for deploy

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const app = require('express')();
-const http = require('http').Server(app);
-const Handler = require('./src/Handler').default
+// const http = require('http').Server(app);
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
