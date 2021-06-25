d8044fee1b64475d7c76d3928d4bf95d70cb7180
Alban Bleicher <alban.b@thewalkingnerds.com>
2021-06-25 13:31:31 +0200

    change socket URL

--- src/index.js
+++ src/index.js
@@ -27,7 +27,7 @@ anim.setSpeed(10);
 
 let socket = false;
 if (!window.location.hash.includes('#nosocket')) {
-  socket = io("ws://localhost:3000");
+  socket = io("https://ixoraa-api.herokuapp.com/");
   console.log('[Socket] Enabled.');
   socket.emit("room create");
   document.addEventListener('DOMContentLoaded', () => {
